a4767bb7f42f52de88c95541ed079c5124ce36ff
aouna <aouna@f216-03.e-im2ag.ujf-grenoble.fr>
2014-11-19 11:53:05 +0100

    modifs abel

--- src/management/Letter.java
+++ src/management/Letter.java
@@ -6,15 +6,27 @@
 
 package management;
 
+
+import env3d.EnvObject;
+import org.lwjgl.input.Keyboard;
+
 /**
  *
  * @author aouna
  */
-public class Letter {
+public class Letter extends EnvObject {
     private char letter;
     
     Letter(char l, double x, double y){
+        setX(x);
+        setY(y);
+        setScale(1);
+        setModel("models/cube/cube.obj");
+        if ( l == ' '){
+            this.letter = ' ';
+            setTexture("models/cube/cube.png");
         
+        }
     }
     
 }
