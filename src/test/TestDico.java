41d6cb492b9ed4f6125e980f3da80cdddd170451
maximiliend <maximilien.douchet@gmail.com>
2014-11-25 23:58:11 +0100

    debut tp9 2

--- src/test/TestDico.java
+++ src/test/TestDico.java
@@ -4,34 +4,12 @@
  * and open the template in the editor.
  */
 
-package test;
-
-import management.Dico;
+package Test;
 
 /**
  *
- * @author douchetm
+ * @author aouna
  */
-
 public class TestDico {
-    public static void main(String[] args) {
-        Dico doci = new Dico("/tmp/lol");
-        
-        System.out.println(doci.addWordToDico(1, "truc"));
-        System.out.println(doci.addWordToDico(1, "lol"));
-        System.out.println(doci.addWordToDico(1, "lol2"));
-        System.out.println(doci.addWordToDico(1, "lol3"));
-        System.out.println(doci.addWordToDico(1, "lol4"));
-        System.out.println(doci.addWordToDico(2, "dur"));
-        
-        System.out.println(doci.getPathToDicoFile());
-        
-        System.out.println(doci.getWordFromListLevel(1));
-
-        System.out.println(doci.getWordFromListLevel(1));
-        System.out.println(doci.getWordFromListLevel(1));
-        System.out.println(doci.getWordFromListLevel(1));
-        
-        System.out.println("2"+doci.getWordFromListLevel(2));
-    }
+    
 }
