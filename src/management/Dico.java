359f4bfe595cdb7dfbba73ab509710114170f857
aouna <aouna@f216-03.e-im2ag.ujf-grenoble.fr>
2014-11-26 10:21:05 +0100

    dico test

--- src/management/Dico.java
+++ src/management/Dico.java
@@ -6,13 +6,24 @@
 
 
 package management;
-import java.util.ArrayList;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 /**
  *
  * @author douchetm
  */
-public class Dico {
+public class Dico extends DefaultHandler {
+    
+    private StringBuffer buffer;
+    private int currentWordLevel;
     private ArrayList<String> listLevel1;
     private ArrayList<String> listLevel2;
     private ArrayList<String> listLevel3;
@@ -20,7 +31,10 @@ public class Dico {
     private ArrayList<String> listLevel5;
     private String pathToDicoFile;
     
+    
+    
     public Dico(String pathToDicoFile) {
+        super();
         this.pathToDicoFile=pathToDicoFile;
         listLevel1 = new ArrayList<String>();
         listLevel2 = new ArrayList<String>();
@@ -65,6 +79,32 @@ public class Dico {
         return mot_choisi;
     }
     
+    public void readDictionnary(String link) {
+        pathToDicoFile = link;
+
+        try {
+            // création d'une fabrique de parseurs SAX
+            SAXParserFactory fabrique = SAXParserFactory.newInstance();
+
+            // création d'un parseur SAX
+            SAXParser parseur = fabrique.newSAXParser();
+
+            // lecture d'un fichier XML avec un DefaultHandler
+            File fichier = new File(pathToDicoFile);
+            parseur.parse(fichier, this);
+
+        } catch (ParserConfigurationException pce) {
+            System.out.println("Erreur de configuration du parseur");
+            System.out.println("Lors de l'appel à newSAXParser()");
+        } catch (SAXException se) {
+            System.out.println("Erreur de parsing");
+            System.out.println("Lors de l'appel à parse()");
+        } catch (IOException ioe) {
+            System.out.println("Erreur d'entrée/sortie");
+            System.out.println("Lors de l'appel à parse()");
+        }
+    }
+    
     public boolean addWordToDico(int level, String word) {
         boolean retour = true;
         switch(level) {
@@ -96,7 +136,96 @@ public class Dico {
         return retour;
     }
     
+     @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+
+        if (qName.equals("dico")) {
+            //rien à faire
+        } else if (qName.equals("mot")) {
+	    //crée un buffer
+            buffer = new StringBuffer();
+
+	    //Récupération d'un attribut
+            int index = attributes.getIndex("niveaudiff");
+            
+            //check l'attribut
+            if (index != -1) {
+		//Transforme le String en int. (Google est mon ami)
+                currentWordLevel = Integer.parseInt(attributes.getValue(index));
+            } else {
+                //pas de level -> erreur XML
+                System.out.println("Erreur dans le XML; le mot est rejeté !");
+            }
+        } else {
+            //erreur, on peut lever une exception
+            throw new SAXException("Balise " + qName + " inconnue.");
+        }
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName)
+            throws SAXException {
+
+        if (qName.equals("dico")) {
+            //rien à faire
+        } else if (qName.equals("word")) {
+	    //on ajoute le mot au dico.
+            
+            
+            addWordToDico(currentWordLevel.toString(),);
+            //on  nettoie le buffer
+            buffer = null;
+        } else {
+            //erreur, on peut lever une exception
+            throw new SAXException("Balise " + qName + " inconnue.");
+        }
+    }
+
+    @Override
+    public void characters(char[] ch, int start, int length)
+            throws SAXException {
+
+	//on récupère le résultat du parse de la méthode characters si celui-ci n'est pas null.
+        String lecture = new String(ch, start, length);
+        if (buffer != null) {
+            buffer.append(lecture);
+        }
+    }
+
+    @Override
+    public void startDocument() throws SAXException {
+        System.out.println("Début du parsing");
+    }
+
+    @Override
+    public void endDocument() throws SAXException {
+        System.out.println("Fin du parsing");
+    }
+    
+    //getters and setters
     public String getPathToDicoFile() {
         return this.pathToDicoFile;
+    }
+
+    public ArrayList<String> getListLevel1() {
+        return listLevel1;
+    }
+
+    public ArrayList<String> getListLevel2() {
+        return listLevel2;
+    }
+
+    public ArrayList<String> getListLevel3() {
+        return listLevel3;
+    }
+
+    public ArrayList<String> getListLevel4() {
+        return listLevel4;
+    }
+
+    public ArrayList<String> getListLevel5() {
+        return listLevel5;
+    }
+
     }
 }
